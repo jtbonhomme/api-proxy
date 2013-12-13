686833553fcbb0bf808d0903c0ddd7b52793fc60
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2013-12-13 19:32:43 +0100

    Cosmetic changes

--- recorder.js
+++ recorder.js
@@ -20,15 +20,15 @@ util.puts(welcome.rainbow.bold);
 
 http.createServer( function( request, response )
 {
-        var url_parts = url.parse( request.url ),
-            options;
+    var url_parts = url.parse( request.url ),
+        options;
 
-        console.log("URL : " + url_parts.href);
-        console.log("PATH : " + url_parts.href.split('/')[1]);
+    console.log("URL : " + url_parts.href);
+    console.log("PATH : " + url_parts.href.split('/')[1]);
 
-        // todo : create an array of hashes [ {ip: "xxx.xxx.xxx.xxx", routes: ["/xxx", "/yyy"] } ] to decide how to serve files
-        switch(url_parts.href.split('/')[1])
-        {
+    // todo : create an array of hashes [ {ip: "xxx.xxx.xxx.xxx", routes: ["/xxx", "/yyy"] } ] to decide how to serve files
+    switch(url_parts.href.split('/')[1])
+    {
             case "":
             case "js":
             case "img":
@@ -51,55 +51,49 @@ http.createServer( function( request, response )
                         headers         : request.headers
                 };
                 break;
-        }
-        console.log("HOST : " + options.hostname);
+    }
+    console.log("HOST : " + options.hostname);
 
-        var request_data;
+    var request_data;
+
+    var proxy_client = http.request( options, function( res )
+    {
+        console.log( 'Sending request ', options );
+
+        res.on( 'data', function ( chunk ) {
+            console.log( 'Write to client ', chunk.length );
+            response.write( chunk, 'binary' );
+            console.log(chunk);
+        });
+
+        res.on( 'end', function() {
+            console.log( 'End chunk write to client' );
+            response.end();
+        });
+
+        res.on( 'error', function ( e ) {
+            console.log( 'Error with client ', e );
+        });
+
+        response.writeHead( res.statusCode, res.headers );
+    });
     
-        var proxy_client = http.request( options, function( res )
-        {
-                console.log( 'Sending request ', options );
+    request.on( 'data', function ( chunk ) {
+        console.log( 'Write to server ', chunk.length );
+        console.log( chunk );
+        console.log( chunk.toString( 'utf8' ) );
+        request_data = request_data + chunk;
+        proxy_client.write( chunk, 'binary' );
+    });
     
-             res.on( 'data', function ( chunk )
-                {
-                        console.log( 'Write to client ', chunk.length );
-                        response.write( chunk, 'binary' );
-                        console.log(chunk);
-                } );
+    request.on( 'end', function() {
+        console.log( 'End chunk write to server' );
+        proxy_client.end();
+    });
     
-             res.on( 'end', function()
-                {
-                        console.log( 'End chunk write to client' );
-                        response.end();
-                } );
-    
-             res.on( 'error', function ( e )
-                {
-                        console.log( 'Error with client ', e );
-                } );
-    
-             response.writeHead( res.statusCode, res.headers );
-        } );
-    
-     request.on( 'data', function ( chunk )
-        {
-                console.log( 'Write to server ', chunk.length );
-                console.log( chunk );
-                console.log( chunk.toString( 'utf8' ) );
-                request_data = request_data + chunk;
-                proxy_client.write( chunk, 'binary' );
-        } );
-    
-     request.on( 'end', function()
-        {
-                console.log( 'End chunk write to server' );
-                proxy_client.end();
-        } );
-    
-     request.on( 'error', function ( e )
-        {
-                console.log( 'Problem with request ', e );
-        } );
+    request.on( 'error', function ( e ) {
+        console.log( 'Problem with request ', e );
+    });
 
 } ).listen( listenPort );
 
